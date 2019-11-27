7d0dd857812dd0e9b21915ea096eb8d09af6540b
Stefan Dieterle <stefand@artcom.de>
2019-11-27 19:52:16 +0100

    Add servings test

---
+++ __tests__/servings-test.js
@@ -0,0 +1,6 @@
+import Servings from "../src/components/servings"
+
+test('Servings component displays servings number', () => {
+    const servings = shallow(<Servings servings={ 3 } />)
+    expect(servings).toHaveText("3 servings")
+})
