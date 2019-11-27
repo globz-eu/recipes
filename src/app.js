d0c18d748c1a4ba2a369bea1a5c34c3d6389db5f
Stefan Dieterle <stefand@artcom.de>
2019-11-27 23:22:46 +0100

    Reduce index, get data in app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
-import React from "react"
+import axios from "axios"
+import React, { useState, useEffect } from "react"
 import Instructions from "./components/instructions"
 import Servings from "./components/servings"
 import Title from "./components/title"
 
-export default props =>
-  <div>
+export default function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    getData(setData)
+  }, ["config.json"])
+  
+  return (
+    <div>
     {
-      props.data.map((datum, i) =>
+      data && data.map((datum, i) =>
         <div key={ i }>
           <Title title={ datum.name } />
           <Servings servings={ datum.servings } />
@@ -15,3 +23,21 @@ export default props =>
       )
     }
   </div>
+  )
+}
+
+async function getData(setData) {
+  const config = await getFromUrl("config.json")
+  const response = await getFromUrl(config.backend)
+  console.log(response)
+  setData(response)
+}
+
+async function getFromUrl(url) {
+  const response =  await axios.get(url).catch(error => new Error(error))
+  if (response instanceof Error) {
+    console.error(response)
+    return
+  }
+  return response.data
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import axios from "axios"
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./app"
 
-async function render() {
-  const configResponse = await axios.get("config.json")
-  const config = configResponse.data
-  const response = await axios.get(config.backend).catch(error => new Error(error))
-  if (response instanceof Error) {
-    console.error(response)
-  } else {
-    console.log(response.data)
-    ReactDOM.render(
-      <App data={ response.data } />,
-      document.getElementById("app")
-    )
-  }
-}
-
-render()
+ReactDOM.render(<App />, document.getElementById('app'))
