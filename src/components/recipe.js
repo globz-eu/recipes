de71fba35325a8919a144eeec8f9cafaf1ba27f9
Stefan Dieterle <stefand@artcom.de>
2019-12-12 12:43:23 +0100

    Layout

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,11 +1,18 @@
 import React from "react"
+import styled from "styled-components"
 import Instructions from "./instructions"
 import Servings from "./servings"
 import Title from "./title"
 
+const Container = styled.div`
+  width: 50vw;
+  margin-bottom: 1em;
+  border: 1px solid;
+`
+
 export default props =>
-  <>
+  <Container>
     <Title title={ props.title } />
     <Servings servings={ props.servings } />
     <Instructions instructions={ props.instructions } />
-  </>
+  </Container>
