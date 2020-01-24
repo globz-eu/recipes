eff40ee399b74003b4f90306f7679e5ed8875ebd
Stefan Dieterle <stefand@artcom.de>
2020-01-24 23:53:41 +0100

    Refactor formHooks tests using enzyme

--- __tests__/formHooks-test.js
+++ __tests__/formHooks-test.js
@@ -1,40 +1,31 @@
 import React from "react"
-import formHook from "../src/formHook"
-import { submit, inputChange } from "../src/handlers"
+import { shallow } from "enzyme"
+import useForm from "../src/formHook"
 
-jest.mock("../src/handlers")
-jest.mock("react")
+function HookWrapper(props) {
+  const hook = props.hook ? props.hook() : undefined
+  return <div hook={ hook } />
+}
 
-describe("formHook", () => {
-  let callback
-  let event
-  let mockInputs
-  let setInputs
+describe("useForm hook", () => {
+  it("wrapper should render", () => {
+    const wrapper = shallow(<HookWrapper />)
 
-  beforeEach(() => {
-    callback = "callback"
-    event = "event"
-    mockInputs = "inputs"
-    setInputs = "setInputs"
-    React.useState.mockImplementation(() => [mockInputs, setInputs])
-    submit.mockImplementation((e, i, c) => ({ e, i, c }))
-    inputChange.mockImplementation((e, i, s) => ({ e, i, s }))
+    expect(wrapper.exists()).toBeTruthy()
   })
 
-  it("should return handleSubmit with the expected signature", () => {
-    const { handleSubmit } = formHook(callback)
-    handleSubmit(event)
-    expect(submit.mock.calls[0]).toEqual(["event", "inputs", "callback"])
+  it("should set initial value", () => {
+    const wrapper = shallow(<HookWrapper hook={ () => useForm(() => {}) } />)
+    const { hook } = wrapper.find("div").props()
+    const { inputs } = hook
+    expect(inputs).toEqual({})
   })
 
-  it("should return handleInputChange with the expected signature", () => {
-    const { handleInputChange } = formHook(callback)
-    handleInputChange(event)
-    expect(inputChange.mock.calls[0]).toEqual(["event", "inputs", "setInputs"])
-  })
-
-  it("should return inputs", () => {
-    const { inputs } = formHook(callback)
-    expect(inputs).toBe(mockInputs)
+  it("should set values on change", () => {
+    const wrapper = shallow(<HookWrapper hook={ () => useForm(() => {}) } />)
+    const { handleInputChange } = wrapper.find("div").props().hook
+    handleInputChange({ target: { name: "title", value: "new" }, persist: () => {} })
+    const { inputs } = wrapper.find("div").props().hook
+    expect(inputs).toEqual({ title: "new" })
   })
 })
