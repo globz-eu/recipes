c27b2eb48748638e3c54807d4e67922c2333bd4d
Stefan Dieterle <stefand@artcom.de>
2020-01-25 00:15:40 +0100

    Add persist and preventDefault mocks

--- __tests__/formHooks-test.js
+++ __tests__/formHooks-test.js
@@ -22,20 +22,26 @@ describe("useForm hook", () => {
   })
 
   it("should set input values on change", () => {
+    const persist = jest.fn()
     const wrapper = shallow(<HookWrapper hook={ () => useForm(() => {}) } />)
     const { handleInputChange } = wrapper.find("div").props().hook
-    handleInputChange({ target: { name: "title", value: "new" }, persist: () => {} })
+    handleInputChange({ target: { name: "title", value: "new" }, persist })
     const { inputs } = wrapper.find("div").props().hook
+    expect(persist.mock.calls.length).toBe(1)
     expect(inputs).toEqual({ title: "new" })
   })
 
   it("should call callback with input values on submit", () => {
     const callback = jest.fn()
+    const persist = jest.fn()
+    const preventDefault = jest.fn()
     const wrapper = shallow(<HookWrapper hook={ () => useForm(callback) } />)
     const { handleInputChange } = wrapper.find("div").props().hook
-    handleInputChange({ target: { name: "title", value: "new" }, persist: () => {} })
+    handleInputChange({ target: { name: "title", value: "new" }, persist })
     const { handleSubmit } = wrapper.find("div").props().hook
-    handleSubmit({ preventDefault: () => {} })
+    handleSubmit({ preventDefault })
+    expect(persist.mock.calls.length).toBe(1)
+    expect(preventDefault.mock.calls.length).toBe(1)
     expect(callback.mock.calls.length).toBe(1)
     expect(callback.mock.calls[0][0]).toEqual({ title: "new" })
   })
