3b124c6861b7d84f5f22328156ab64bd7899bc3c
Stefan Dieterle <stefand@artcom.de>
2020-01-25 00:09:51 +0100

    Add submit tests

--- __tests__/formHooks-test.js
+++ __tests__/formHooks-test.js
@@ -14,18 +14,29 @@ describe("useForm hook", () => {
     expect(wrapper.exists()).toBeTruthy()
   })
 
-  it("should set initial value", () => {
+  it("should set initial input value", () => {
     const wrapper = shallow(<HookWrapper hook={ () => useForm(() => {}) } />)
     const { hook } = wrapper.find("div").props()
     const { inputs } = hook
     expect(inputs).toEqual({})
   })
 
-  it("should set values on change", () => {
+  it("should set input values on change", () => {
     const wrapper = shallow(<HookWrapper hook={ () => useForm(() => {}) } />)
     const { handleInputChange } = wrapper.find("div").props().hook
     handleInputChange({ target: { name: "title", value: "new" }, persist: () => {} })
     const { inputs } = wrapper.find("div").props().hook
     expect(inputs).toEqual({ title: "new" })
+  })
+
+  it("should call callback with input values on submit", () => {
+    const callback = jest.fn()
+    const wrapper = shallow(<HookWrapper hook={ () => useForm(callback) } />)
+    const { handleInputChange } = wrapper.find("div").props().hook
+    handleInputChange({ target: { name: "title", value: "new" }, persist: () => {} })
+    const { handleSubmit } = wrapper.find("div").props().hook
+    handleSubmit({ preventDefault: () => {} })
+    expect(callback.mock.calls.length).toBe(1)
+    expect(callback.mock.calls[0][0]).toEqual({ title: "new" })
   })
 })
