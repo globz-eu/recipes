6ca4c915b6c2a3f7aee67610fdf8f1988405dd91
Stefan Dieterle <stefand@artcom.de>
2020-07-28 23:57:17 +0200

    Adapt requests tests

--- __tests__/requests-test.js
+++ __tests__/requests-test.js
@@ -2,23 +2,27 @@ import { rest } from "msw"
 import { setupServer } from "msw/node"
 import { getFromUrl, postToUrl } from "../src/requests"
 
-const config = { backend: "http://localhost:8000/recipes/" }
+const config = { backend: "https://recipes.eu/recipes" }
+const data = { data: "data" }
 
 const server = setupServer(
   rest.get("/config.json", (req, res, ctx) => res(
-    ctx.json(config),
+    ctx.json(config), ctx.status(200),
+  )),
+  rest.post(config.backend, (req, res, ctx) => res(
+    ctx.json(data), ctx.status(201)
   )),
 )
 
+beforeAll(() => {
+  server.listen()
+})
+
+afterAll(() => {
+  server.close()
+})
+
 describe("getFromUrl", () => {
-  beforeAll(() => {
-    server.listen()
-  })
-
-  afterAll(() => {
-    server.close()
-  })
-
   it("returns the correct data", async () => {
     const response = await getFromUrl("config.json")
     expect(response).toEqual(config)
@@ -27,7 +31,7 @@ describe("getFromUrl", () => {
   it("catches errors", async () => {
     server.use(
       rest.get("/config.json", (req, res, ctx) => res.once(
-        ctx.status(500),
+        ctx.status(404),
       )),
     )
     const response = await getFromUrl("config.json")
@@ -36,22 +40,18 @@ describe("getFromUrl", () => {
 })
 
 describe("postToUrl", () => {
-  const postData = { data: "data" }
-  const postResponse = { data: { someData: "data" } }
-
-  beforeEach(() => {
-    axios.post = jest.fn().mockResolvedValue(postResponse)
-  })
-
   it("returns the expected response", async () => {
-    const response = await postToUrl(config.backend, postData)
-    expect(response).toEqual(postResponse.data)
-    expect(axios.post).toBeCalledWith(config.backend, postData)
+    const response = await postToUrl(config.backend, data)
+    expect(response).toEqual(data)
   })
 
   it("catches errors", async () => {
-    axios.post.mockRejectedValue()
-    const response = await postToUrl(config.backend, postData)
+    server.use(
+      rest.post(config.backend, (req, res, ctx) => res.once(
+        ctx.status(400)
+      ))
+    )
+    const response = await postToUrl(config.backend, data)
     expect(response).toEqual(null)
   })
 })
