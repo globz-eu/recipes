26a55268f0cb5ec84a8251225c64be5e3ceddd25
Stefan Dieterle <stefand@artcom.de>
2020-08-04 23:45:06 +0200

    Add profile test

---
+++ __tests__/components/profile-test.js
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react"
+import React from "react"
+import { useAuth0 } from "@auth0/auth0-react"
+import Profile from "../../src/components/profile"
+
+jest.mock("@auth0/auth0-react")
+
+describe("Profile", () => {
+  const mockLogout = jest.fn()
+  const user = {
+    email: "johndoe@me.com",
+    name: "John Doe",
+    email_verified: true, // eslint-disable-line camelcase
+    sub: "google-oauth2|2147627834623744883746",
+  }
+
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: mockLogout,
+      loginWithRedirect: jest.fn(),
+    })
+  })
+
+  it("displays the user name when authenticated", () => {
+    render(<Profile />)
+    expect(screen.getByText("John Doe")).toBeInTheDocument()
+  })
+
+  it("displays the user email when authenticated", () => {
+    render(<Profile />)
+    expect(screen.getByText("johndoe@me.com")).toBeInTheDocument()
+  })
+})
