90f98e08f4f24f44e8a6b840cbc650287c7d62dd
Stefan Dieterle <stefand@artcom.de>
2020-08-05 00:47:17 +0200

    Adapt app tests

--- __tests__/app-test.js
+++ __tests__/app-test.js
@@ -1,8 +1,11 @@
 import { render, screen, waitForElementToBeRemoved } from "@testing-library/react"
 import React from "react"
+import { useAuth0 } from "@auth0/auth0-react"
 import App from "../src/app"
-import { server } from "../mockApi/api"
+import { server, config } from "../mockApi/api"
 import recipes from "../testData/recipes.json"
+
+jest.mock("@auth0/auth0-react")
 
 beforeAll(() => {
   server.listen()
@@ -12,9 +15,32 @@ afterAll(() => {
   server.close()
 })
 
-describe("App", () => {
+describe("App, when user is authenticated", () => {
+  const user = {
+    email: "johndoe@me.com",
+    name: "John Doe",
+    email_verified: true, // eslint-disable-line camelcase
+    sub: "google-oauth2|2147627834623744883746",
+  }
+
   beforeEach(() => {
-    render(<App />)
+    useAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      getAccessTokenSilently: jest.fn()
+    })
+    render(<App config={ config } />)
+  })
+
+  it("contains the log out button", async () => {
+    await waitForElementToBeRemoved(screen.queryByText("Loading ..."))
+    expect(screen.getByText("Log Out")).toBeInTheDocument()
+  })
+
+  it("contains the user profile", async () => {
+    await waitForElementToBeRemoved(screen.queryByText("Loading ..."))
+    expect(screen.getByText("John Doe")).toBeInTheDocument()
+    expect(screen.getByText("johndoe@me.com")).toBeInTheDocument()
   })
 
   it("contains the expected recipe titles", async () => {
@@ -43,5 +69,22 @@ describe("App", () => {
     expect(screen.getByLabelText("Title")).toBeInTheDocument()
     expect(screen.getByLabelText("Servings")).toBeInTheDocument()
     expect(screen.getByLabelText("Instructions")).toBeInTheDocument()
+  })
+})
+
+describe("App, when user is not authenticated", () => {
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: false
+    })
+    render(<App config={ config } />)
+  })
+
+  it("contains the login button", async () => {
+    expect(screen.getByText("Log In")).toBeInTheDocument()
+  })
+
+  it("contains the please log in hint", () => {
+    expect(screen.getByText("Please log in ...")).toBeInTheDocument()
   })
 })
