a43ef778b4cce9cdda56e334d36adc33ab1fa82a
Stefan Dieterle <stefand@artcom.de>
2021-01-02 16:44:20 +0100

    Use recipeForm for recipe detail

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
 import Loading from "./components/loading"
+import RecipeForm from "./components/recipeForm"
 import RecipeList from "./components/recipeList"
 
 export default props => {
   const [data, setData] = useState(null)
+  const [recipe, setRecipe] = useState(null)
   React.useEffect(() => {
     async function updateData() {
       const updatedData = await props.getLatestData(props.config.backend)
@@ -15,7 +17,24 @@ export default props => {
   return (
     <div>
       <Loading loading={ data == null } />
-      <RecipeList data={ data } />
+      {
+        recipe != null
+          ? <RecipeForm onSubmit={ () => {} } recipe={ recipe } onClose={ () => setRecipe(null) } />
+          : ( // eslint-disable-line no-extra-parens
+            <RecipeList
+              data={ data }
+              recipeDetail={
+                id =>
+                  recipeDetail(id, props.getRecipeById, setRecipe, props.config)
+              }
+              onClose={ () => setRecipe(null) } />
+          )
+      }
     </div>
   )
+}
+
+async function recipeDetail(id, getRecipeById, setRecipe, config) {
+  const recipe = await getRecipeById(config.backend, id)
+  setRecipe(recipe)
 }
--- src/components/recipeList.js
+++ src/components/recipeList.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Title from "./title"
 
-export default ({ data, }) =>
+export default ({ data, recipeDetail }) =>
   <>
     {
       data && data.recipes && data.recipes.map((datum, i) =>
         <Title
           key={ i }
           title={ datum.name }
-          id={ datum.id } />
+          onClick={ () => recipeDetail(datum.id) } />
       )
     }
   </>
