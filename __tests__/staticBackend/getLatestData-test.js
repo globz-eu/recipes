8cbbe9a58982d04ecef840e6fb54c2452cd11911
Stefan Dieterle <stefand@artcom.de>
2021-01-04 16:35:13 +0100

    Fix getLatestData test for static backend

--- __tests__/staticBackend/getLatestData-test.js
+++ __tests__/staticBackend/getLatestData-test.js
@@ -12,16 +12,22 @@ afterAll(() => {
 
 describe("getLatestData", () => {
   it("returns the expected local data", async () => {
-    const recipesNames = recipes.map(recipe => ({ id: recipe.id, name: recipe.name }))
+    const recipeList = recipes.map(recipe => ({ id: recipe.id, name: recipe.name }))
     const data = await getLatestData(config.backend)
-    expect(data).toEqual({ recipes: recipesNames })
+    expect(data).toEqual({ recipes: recipeList })
+  })
+
+  it("stores recipes in sessionStorage", async () => {
+    await getLatestData(config.backend)
+    const storedRecipes = JSON.parse(window.sessionStorage.getItem("recipes"))
+    expect(storedRecipes).toEqual(recipes)
   })
 })
 
 describe("getRecipeById", () => {
   it("returns the expected recipe data", async () => {
     const recipe = recipes[0]
-    const data = await getRecipeById(config.backend, 0)
+    const data = await getRecipeById({ id: 0 })
     expect(data).toEqual(recipe)
   })
 })
